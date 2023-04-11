2fe01fd193d1e5cd2cebd6f1da50cab283653e9f
Elijah Mooring <vehmloewff@gmail.com>
2023-04-11 13:02:49 -0700

    chore: remove breaking dependencies

--- bundle/deps.ts
+++ bundle/deps.ts
@@ -9,17 +9,7 @@ import * as esBuildNative from 'https://deno.land/x/esbuild@v0.15.13/mod.js'
 export * as esBuildTypes from 'https://deno.land/x/esbuild@v0.15.13/mod.d.ts'
 export const esBuild = Deno.run === undefined ? esBuildWasm : esBuildNative
 
-export { sh } from '../../../Vehmloewff/dtils/lib/sh.ts'
-export { writeText } from 'https://deno.land/x/dtils@1.6.1/lib/fs.ts'
-
-export { bundle as denoBundle } from 'https://deno.land/x/emit@0.7.0/mod.ts'
 export { createCache, type Loader } from 'https://deno.land/x/deno_cache@0.4.1/mod.ts'
-
-export { serve as createHttpServer } from 'https://deno.land/std@0.165.0/http/mod.ts'
-export { serveDir } from 'https://deno.land/std@0.165.0/http/file_server.ts'
-
-export { createWatcher } from 'https://code.jikno.com/rumble@next/utils/watcher.ts'
-
 export { compile as compileSvelte, preprocess as preprocessSvelte } from 'https://cdn.jsdelivr.net/npm/svelte@3.49.0/compiler.mjs'
 
 export { denoPlugin } from 'https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts'
