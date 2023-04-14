66d53034408c196a58f4a2639ab900ac14b65943
Elijah Mooring <vehmloewff@gmail.com>
2023-04-14 09:58:32 -0700

    feat: env should have a typing

--- lib/env.ts
+++ lib/env.ts
@@ -1,8 +1,8 @@
 export type Env = 'dev' | 'staging' | 'production'
 
-export let env = getEnv()
+export let env: Env = getEnv()
 
-function getEnv() {
+function getEnv(): Env {
 	const env = Deno.env.get('ENV') || 'dev'
 	if (env !== 'dev' && env !== 'staging' && env !== 'production') throw new Error(`Invalid ENV value: ${env}`)
 
