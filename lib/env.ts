ee84f8bdcdbdbafef7510d6cae0314e6aaa09797
Elijah Mooring <vehmloewff@gmail.com>
2023-05-22 18:20:35 -0700

    ref: updates to env

--- lib/env.ts
+++ lib/env.ts
@@ -1,14 +1,33 @@
 export type Env = 'dev' | 'staging' | 'production'
 
-export let env: Env = getEnv()
+const stashedEnvs = new Map<string, Env>()
+let lookedAtEnvVar = false
 
-function getEnv(): Env {
-	const env = Deno.env.get('ENV') || 'dev'
-	if (env !== 'dev' && env !== 'staging' && env !== 'production') throw new Error(`Invalid ENV value: ${env}`)
+export function getEnv(scope: string): Env {
+	const stashedEnv = stashedEnvs.get(scope)
+	if (stashedEnv) return stashedEnv
 
-	return env
+	if (!lookedAtEnvVar) {
+		const envVarString = Deno.env.get('ENV')
+		if (envVarString) populateEnvString(envVarString)
+
+		lookedAtEnvVar = true
+		return getEnv(scope)
+	}
+
+	stashedEnvs.set(scope, 'production')
+	return 'production'
 }
 
-export function setEnv(newEnv: Env) {
-	env = newEnv
+function populateEnvString(env: string) {
+	const sections = env.split(',').map((str) => str.trim())
+
+	for (const [sectionScope, sectionEnv] of sections) {
+		if (!sectionEnv) throw new Error(`Expected section in ENV var to be a key=value pair, but found: "${sectionScope}"`)
+		if (sectionEnv !== 'dev' && sectionEnv !== 'staging' && sectionEnv !== 'production') {
+			throw new Error(`Invalid ENV value for key ${sectionScope}: ${env}`)
+		}
+
+		stashedEnvs.set(sectionScope, sectionEnv)
+	}
 }
