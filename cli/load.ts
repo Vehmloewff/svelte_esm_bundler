059010530cde4fd851ec48f68e6a8559ca55d419
Elijah Mooring <vehmloewff@gmail.com>
2023-05-22 18:26:43 -0700

    feat: check devops/mod.ts for an entry file

--- cli/load.ts
+++ cli/load.ts
@@ -1,8 +1,7 @@
 import { Action } from './strategy.ts'
 
 export async function loadDevopsFile() {
-	const module = await import(`file://${Deno.cwd()}/devops.ts`)
-
+	const module = await import(await getFileUrl())
 	const exposedTasks = Object.keys(module)
 
 	const runAction = async (action: Action) => {
@@ -13,4 +12,25 @@ export async function loadDevopsFile() {
 	}
 
 	return { exposedTasks, runAction }
+}
+
+async function getFileUrl() {
+	const exists = async (path: string) => {
+		try {
+			await Deno.stat(path)
+			return true
+		} catch (_) {
+			return false
+		}
+	}
+
+	const wrapPath = (path: string) => `file://${Deno.cwd()}/${path}`
+
+	const tryPaths = ['devops.ts', 'devops/mod.ts']
+
+	for (const path of tryPaths) {
+		if (await exists(path)) return wrapPath(path)
+	}
+
+	throw new Error(`Could not find a devops entrypoint. Tried ${tryPaths.join(', ')}`)
 }
